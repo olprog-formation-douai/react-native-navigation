57c0170f6cbd68d513f0ac9132ee863a4c9eab2a
olprog <samuel.michaux@gmail.com>
2022-03-30 11:32:48 +0200

    first commit with react-navigation

---
+++ components/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <Text style={{fontSize: 30}}>Home Screen</Text>
+    );
+};
+
+export default HomeScreen;
